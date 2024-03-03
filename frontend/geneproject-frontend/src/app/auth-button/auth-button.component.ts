8dfdff384d9662cf3deee1e1e072dedbd3130817
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2024-03-03 01:07:56 -0500

    add logout option

--- frontend/geneproject-frontend/src/app/auth-button/auth-button.component.ts
+++ frontend/geneproject-frontend/src/app/auth-button/auth-button.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+import { Inject} from '@angular/core';
 @Component({
   selector: 'app-auth-button',
   templateUrl: './auth-button.component.html',
   styleUrl: './auth-button.component.css'
 })
 export class AuthButtonComponent {
-  constructor(public auth: AuthService) {}
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+  
   loginWithRedirect(){
     this.auth.loginWithRedirect()
+    console.log(this.auth.isAuthenticated$)
   }
+  logout(){
+    this.auth.logout({ logoutParams: { returnTo: document.location.origin } })
+   }
 }
