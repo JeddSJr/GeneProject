e614647f50ac18e25429e475ac2d2af2678d57b6
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2024-03-03 00:28:30 -0500

    stable and working Auth0 authentif

---
+++ frontend/geneproject-frontend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AuthModule } from '@auth0/auth0-angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AuthButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-jz7lwt5lmlxvurxt.us.auth0.com',
+      clientId: 'FPoqV6Tf7mvnhroEZ5wqSOhbrs3G0P2k',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
